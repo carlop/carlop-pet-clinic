062be550845a13d1ecabf6ad3fe2db135c66412d
Carlos López <yo@carlop.es>
2019-02-28 21:47:56 +0100

    Adding POJOs. Closes #2.

---
+++ src/main/java/es/carlop/petclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package es.carlop.petclinic.model;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/es/carlop/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package es.carlop.petclinic.model;
+
+public class Vet extends Person {
+}
