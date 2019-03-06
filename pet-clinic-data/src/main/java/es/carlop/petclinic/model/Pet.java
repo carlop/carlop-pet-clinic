f4300bbafdbda759e7985b82711cd85f09a481ea
Carlos López <yo@carlop.es>
2019-03-06 15:57:44 +0100

    Added base entity. Closes #10.

--- pet-clinic-data/src/main/java/es/carlop/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/es/carlop/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package es.carlop.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/es/carlop/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/es/carlop/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package es.carlop.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
