5c5954593a5b91d749dd746773be5db25a96bfa0
Carlos López <yo@carlop.es>
2019-03-06 15:40:49 +0100

    Implement Interfaces for Services. Closes #5.

---
+++ pet-clinic-data/src/main/java/es/carlop/petclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package es.carlop.petclinic.services;
+
+import es.carlop.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
---
+++ pet-clinic-data/src/main/java/es/carlop/petclinic/services/VetService.java
@@ -0,0 +1,17 @@
+package es.carlop.petclinic.services;
+
+import es.carlop.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findByLastName(String lastName);
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+
+}
