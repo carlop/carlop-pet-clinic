91e8642fd428747325f8e84269ab24e33640c8ec
Carlos López <yo@carlop.es>
2019-03-06 16:36:33 +0100

    Implement Map based services. Closes #6.

---
+++ pet-clinic-data/src/main/java/es/carlop/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package es.carlop.petclinic.services.map;
+
+import es.carlop.petclinic.model.Owner;
+import es.carlop.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
