29eba38a5e6b9ba1795d7bf2cb850709a3ddd405
Carlos López <yo@carlop.es>
2019-03-09 19:29:09 +0100

    Added Speciality Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/es/carlop/petclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package es.carlop.petclinic.services;
+
+import es.carlop.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/es/carlop/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,33 @@
+package es.carlop.petclinic.services.map;
+
+import es.carlop.petclinic.model.Speciality;
+import es.carlop.petclinic.services.SpecialityService;
+
+import java.util.Set;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
