6827fa92bed40dd83aae7741787ba9f209be41b1
Carlos López <yo@carlop.es>
2019-03-11 19:14:25 +0100

    Added MockMvc Tests for Owner Controller. Closes #51.

--- pet-clinic-web/src/main/java/es/carlop/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/es/carlop/petclinic/controllers/OwnerController.java
@@ -23,4 +23,11 @@ public class OwnerController {
         return "owners/index";
     }
 
+    @RequestMapping("/find")
+    public String findOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
+
+        return "owners/index";
+    }
+
 }
